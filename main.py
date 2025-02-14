62ee281eaa9a5395d6aca207dafed10ba5919323
Timothy Sipkens <tsipkens@gmail.com>
2025-02-13 23:09:06 -0500

    Update main for load_imgs update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import tools
 import agg, pp
 
 # Load images from the 'images' directory
-Imgs, imgs, pixsizes = tools.load_imgs('images')  # OPTION 3: load all images in 'images' folder
+imgs, pixsizes = tools.load_imgs('images')  # OPTION 3: load all images in 'images' folder
 
 # Run K-MEANS for all images
 imgs_binary, img_kmeans, feature_set = agg.seg_kmeans(imgs, pixsizes)
